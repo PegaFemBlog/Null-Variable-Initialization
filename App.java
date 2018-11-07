6cc9286edf57205dbadaa3486c65d697550ea72a
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-11-07 16:45:18 -0600

    Initial upload

---
+++ App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.codealongpersonmethods;
+
+/**
+ *
+ * @author bacag
+ */
+public class App {
+    public static void main(String[] args) {
+        int count = 19;
+        Person person = new Person();
+        
+        System.out.println("Count ="+ count);
+        System.out.println("Age =" + person.getAge());
+    }
+    
+}
---
+++ Person.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.codealongpersonmethods;
+
+/**
+ *
+ * @author bacag
+ */
+public class Person {
+    private int age;    
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge() {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName() {
+        this.name = name;
+    }
+}
